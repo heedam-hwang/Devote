27f4cba288f2a700497fa34ed3e8e82757c4b474
heedam hwang <heedam21@gmail.com>
2022-02-05 01:16:14 +0900

    feat: UI 개선(배경 이미지, 배경 색상, 리스트 형태 변경)

---
+++ Devote/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  Devote
+//
+//  Created by 황희담 on 2022/02/05.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     
     // FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
@@ -43,11 +43,11 @@ struct ContentView: View {
             hideKeyboard()
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -56,64 +56,80 @@ struct ContentView: View {
             }
         }
     }
-
-
+    
+    
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            VStack {
-                VStack(spacing: 16) {
-                    TextField("New Task", text: $task)
+            ZStack {
+                VStack {
+                    VStack(spacing: 16) {
+                        TextField("New Task", text: $task)
+                            .padding()
+                            .background(
+                                Color(.systemGray6)
+                            )
+                            .cornerRadius(10)
+                        Button {
+                            addItem()
+                        } label: {
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
+                        }
+                        .disabled(isButtonDisabled)
                         .padding()
-                        .background(
-                            Color(.systemGray6)
-                        )
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .background(isButtonDisabled ? Color.gray : Color.pink)
                         .cornerRadius(10)
-                    Button {
-                        addItem()
-                    } label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    }
-                    .disabled(isButtonDisabled)
+                    } //: VSTACK
                     .padding()
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .background(isButtonDisabled ? Color.gray : Color.pink)
-                    .cornerRadius(10)
+                    
+                    
+                    
+                    List {
+                        ForEach(items) { item in
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            } //: LIST ITEM
+                        }
+                        .onDelete(perform: deleteItems)
+                    } //: LIST
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
                 } //: VSTACK
-                .padding()
-                
-                
-            
-            List {
-                ForEach(items) { item in
-                    VStack(alignment: .leading) {
-                        Text(item.task ?? "")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                            .font(.footnote)
-                            .foregroundColor(.gray)
-                    } //: LIST ITEM
-                }
-                .onDelete(perform: deleteItems)
-            } //: LIST
-        } //: VSTACK
+            } // ZSTACK
+            .onAppear(perform: {
+                UITableView.appearance().backgroundColor = UIColor.clear
+            })
             .navigationTitle("Daily Tasks")
             .navigationBarTitleDisplayMode(.large)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                EditButton()
-            }
-            ToolbarItem {
-                Button(action: addItem) {
-                    Label("Add Item", systemImage: "plus")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
                 }
-            }
-        } //: TOOLBAR
-    } // : NAVIGATION
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            } //: TOOLBAR
+            .background(
+                BackgroundImageView()
+            )
+            .background(
+                backgroundGradient.ignoresSafeArea(.all)
+            )
+        } // : NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
