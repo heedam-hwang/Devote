fda07fe9ad39bfec0fe1df0d65ca514ba5a83c9d
heedam hwang <heedam21@gmail.com>
2022-02-04 22:41:21 +0900

    initial commit

---
+++ Devote/Utility/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Devote
+//
+//  Created by 황희담 on 2022/02/04.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
